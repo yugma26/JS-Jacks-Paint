4b6dca0dd6951dd1cac3c6565859478779b0668e
yugma26 <66039476+yugma26@users.noreply.github.com>
2022-10-12 18:21:51 -0400

    Add files via upload

---
+++ paint.js
@@ -0,0 +1,104 @@
+function configureListeners() {
+    let images = document.querySelectorAll("img")// select img elements  
+
+
+     for (var i = 0; i < images.length; i++) {        
+        let getImage= document.getElementById(images[i].id) //get the different images by the CSS Id.
+
+        getImage.addEventListener('mouseover', addOpacity, false) //assign mouseover event, then add Opacity.       
+        getImage.addEventListener('mouseout', removeOpacity, false)//assign mouseout event, the remove Opacity.        
+    } // iterate over images and add mouseover event listeners      
+    
+}
+
+function addOpacity(event) {
+    
+    if (!this.classList.contains('dim')){
+        this.classList.add('dim')
+        }  // add appropriate CSS class
+    getProductInfo(event.target.id);     
+}
+
+function removeOpacity(event) {
+    if (this.classList.contains('dim')){
+        this.classList.remove('dim')
+        }//remove appropriate CSS class
+
+    let element = document.getElementById('color-price');
+        element.textContent = '';
+        
+    let color = document.getElementById('color-name');
+        color.textContent = ''; 
+
+    event.preventDefault();    
+}
+
+function getProductInfo(paintColor) {
+    let price;
+    let colorName;  
+    
+    switch (paintColor) {
+        case 'pn1':           
+        price = '$14.99'  
+        colorName = 'Lime Green'
+        updatePrice(colorName, price)
+        break; 
+
+    case 'pn2':
+        price = '$11.14'  
+        colorName = 'Medium Brown'
+        updatePrice(colorName, price)
+         break;
+
+    case 'pn3':
+        price = '$22.99'  
+        colorName = 'Royal Blue'
+        updatePrice(colorName, price)
+        break;   
+    case 'pn4':
+        price = '4.99'  
+        colorName = 'Solid Black'
+        updatePrice(colorName, price)
+        break;
+
+    case 'pn5':
+        price = '$8.22'  
+        colorName = 'Solid Cyan'
+        updatePrice(colorName, price)   
+        break;
+
+    case 'pn6':
+        price = '$11.99'  
+        colorName = 'Solid Purple'
+        updatePrice(colorName, price)        
+        break;
+
+    case 'pn7':
+        price = '$13.42'  
+        colorName = 'Solid Red'
+        updatePrice(colorName, price)
+        break;   
+    case 'pn8':
+        price = '$21.89'  
+        colorName = 'Solid White'
+        updatePrice(colorName, price)
+        break;
+
+    case 'pn9':
+        price = '$14.99'  
+        colorName = 'Lime Green'
+        updatePrice(colorName, price)
+        break;   
+      default:              
+}
+
+    function updatePrice(colorName, price)
+    {       
+        let colorPrice = document.getElementById('color-price');//h2 tag id="color-price" price to be inserted.
+        colorPrice.textContent = price;
+        
+        let color = document.getElementById('color-name');//h2 tag id = "color-name" color to be inserted.
+        color.textContent = colorName; 
+    }
+    
+}
